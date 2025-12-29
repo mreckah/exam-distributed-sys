26676280c6bdc586aeb6c4e2b63d3a9083d23420
mreckah <m.reckahwalt@gmail.com>
2025-12-29 08:59:01 +0100

    project setup

---
+++ src/main/java/com/oussama/event_sourcing/EventSourcingApplication.java
@@ -0,0 +1,13 @@
+package com.oussama.event_sourcing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EventSourcingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EventSourcingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oussama/event_sourcing/EventSourcingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oussama.event_sourcing;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventSourcingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
